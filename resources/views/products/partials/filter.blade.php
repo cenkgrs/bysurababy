ae47d12d728e426f959ea924ca96cc1460d43a46
cenkgrs <cenkgrs@gmail.com>
2021-08-19 04:05:13 +0300

    Css changes

--- resources/views/products/partials/filter.blade.php
+++ resources/views/products/partials/filter.blade.php
@@ -1,75 +1,77 @@
 <form action="{{ route('products') }}" method="post">
     {!! csrf_field() !!}
 
-    <div class="row">
-    <h5 class="color-primary">Filtreler</h5>
+    <div class="filter">
+        <div class="row">
+            <h5 class="color-primary">Filtreler</h5>
 
-    <div class="col-lg-12">
+            <div class="col-lg-12">
 
-        <div class="filter-category">
-            <h6>Kategori</h6>
-            <div class="row mt1">
-                <div class="col-lg-12">
-                    @if (Request::input('category') || Request::input('sub_category'))
-                        <ul>
-                            <li><input type="checkbox" class="category-checkbox" name="category" value="{{ $category->id }}" checked>{{ $category->name }}
+                <div class="filter-category">
+                    <h6>Kategori</h6>
+                    <div class="row mt1">
+                        <div class="col-lg-12">
+                            @if (Request::input('category') || Request::input('sub_category'))
                                 <ul>
-                                    @foreach ($category->subCategories as $sub_cat)
-                                        <li><input type="checkbox" class="sub-category-checkbox" name="sub_category" {{ Request::input('sub_category') == $sub_cat->id ? 'checked' : '' }} value="{{ $sub_cat->id }}">{{ $sub_cat->name }}
-                                    @endforeach
+                                    <li><input type="checkbox" class="category-checkbox" name="category" value="{{ $category->id }}" checked>{{ $category->name }}
+                                        <ul>
+                                            @foreach ($category->subCategories as $sub_cat)
+                                                <li><input type="checkbox" class="sub-category-checkbox" name="sub_category" {{ Request::input('sub_category') == $sub_cat->id ? 'checked' : '' }} value="{{ $sub_cat->id }}">{{ $sub_cat->name }}
+                                            @endforeach
+                                        </ul>
+                                    </li>
                                 </ul>
-                            </li>
-                        </ul>
-                    @else
-                        <ul>
-                            @foreach ($categories as $cat)
-                                <li><input type="checkbox" class="category-checkbox" name="category" value="{{ $cat->id }}">{{ $cat->name }}
-                                    <ul>
-                                        @foreach ($cat->subCategories as $sub_cat)
-                                            <li><input type="checkbox" class="sub-category-checkbox" name="sub_category" value="{{ $sub_cat->id }}">{{ $sub_cat->name }}</li>
-                                        @endforeach
-                                    </ul>
-                                </li>
-                            @endforeach
+                            @else
+                                <ul>
+                                    @foreach ($categories as $cat)
+                                        <li><input type="checkbox" class="category-checkbox" name="category" value="{{ $cat->id }}">{{ $cat->name }}
+                                            <ul>
+                                                @foreach ($cat->subCategories as $sub_cat)
+                                                    <li><input type="checkbox" class="sub-category-checkbox" name="sub_category" value="{{ $sub_cat->id }}">{{ $sub_cat->name }}</li>
+                                                @endforeach
+                                            </ul>
+                                        </li>
+                                    @endforeach
 
 
-                        </ul>
-                    @endif
+                                </ul>
+                            @endif
 
 
+                        </div>
+                    </div>
                 </div>
+
+                <div class="filter-price">
+                    <h6>Fiyat Aralığı</h6>
+                    <div class="row mt1">
+                        <div class="col-lg-6 col-6">
+                            <input type="text" name="price-min" class="form-control" placeholder="En Az" value="{{ Request::input('price-min') ?? '' }}">
+                        </div>
+                        <div class="col-lg-6 col-6">
+                            <input type="text" name="price-max" class="form-control" placeholder="En Çok" value="{{ Request::input('price-max') ?? '' }}">
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="filter-gender">
+                    <h6>Cinsiyet</h6>
+                    <div class="row mt1">
+                        <div class="col-lg-12">
+                            <ul>
+                                <li><input type="checkbox" name="gender" {{ Request::input('gender') == 'male' ? 'checked' : ''}} value="male">Erkek</li>
+                                <li><input type="checkbox" name="gender" {{ Request::input('gender') == 'female' ? 'checked' : ''}} value="female">Kız</li>
+                                <li><input type="checkbox" name="gender" {{ Request::input('gender') == 'unisex' ? 'checked' : ''}} value="unisex">Unisex</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <button class="btn primary-button" type="submit">Uygula</button>
+
             </div>
         </div>
-
-        <div class="filter-price">
-            <h6>Fiyat Aralığı</h6>
-            <div class="row mt1">
-                <div class="col-lg-6">
-                    <input type="text" name="price-min" class="form-control" placeholder="En Az" value="{{ Request::input('price-min') ?? '' }}">
-                </div>
-                <div class="col-lg-6">
-                    <input type="text" name="price-max" class="form-control" placeholder="En Çok" value="{{ Request::input('price-max') ?? '' }}">
-                </div>
-                <div class="col-lg-12 mt1">
-                    <button class="btn primary-button" type="submit">Uygula</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="filter-gender">
-            <h6>Cinsiyet</h6>
-            <div class="row mt1">
-                <div class="col-lg-12">
-                    <ul>
-                        <li><input type="checkbox" name="gender" {{ Request::input('gender') == 'male' ? 'checked' : ''}} value="male">Erkek</li>
-                        <li><input type="checkbox" name="gender" {{ Request::input('gender') == 'female' ? 'checked' : ''}} value="female">Kız</li>
-                        <li><input type="checkbox" name="gender" {{ Request::input('gender') == 'unisex' ? 'checked' : ''}} value="unisex">Unisex</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
     </div>
-</div>
 
 </form>
