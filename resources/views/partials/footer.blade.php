55d1ad1f6e2323c8a23c9325f3b2e5b2a6b14b26
cenkgrs <cenkgrs@gmail.com>
2021-08-21 03:42:14 +0300

    Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,34 @@
+<div class="footer-clean">
+    <footer>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>bySura baby</h3>
+                    <ul>
+                        <li><a href="#"><span>Ürünler</span></a></li>
+                        <li><a href="#"><span>Kategoriler</span></a></li
+                        <li><a href="#"><span>İletişim</span></a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Hakkımızda</h3>
+                    <ul>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Vizyon</a></li>
+                        <li><a href="#">Sıkça Sorulan Sorular</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4 col-md-3 item">
+                    <h3>Üyelik</h3>
+                    <ul>
+                        <li><a href="#">Hesabım</a></li>
+                        <li><a href="#">Siparişlerim</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 item social"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-instagram"></i></a>
+                    <p class="copyright">Company Name © 2018</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
--- resources/views/products/partials/list.blade.php
+++ resources/views/products/partials/list.blade.php
@@ -1,24 +1,22 @@
 <div class="row">
 @foreach ($products as $product)
     <div class="col-lg-4">
-        <a href="{{ route('product', [$product->id]) }}">
-            <div class="product">
-                <div class="product-image">
-                    <img src="{{ asset('/images/products/'.$product->id.'.jpg') }}" alt="">
+        <div class="product" data-href="{{ route('product', $product->id)}}">
+            <div class="product-image">
+                <img src="{{ asset('/images/products/'.$product->id.'.jpg') }}" alt="">
+            </div>
+            <div class="product-title">
+                {{ $product->name }}
+            </div>
+            <div class="product-summary">
+                <div class="product-price">
+                    {{ number_format((float)$product->price->sale_price, 2, '.', '') }} ₺
                 </div>
-                <div class="product-title">
-                    {{ $product->name }}
-                </div>
-                <div class="product-summary">
-                    <div class="product-price">
-                        {{ number_format((float)$product->price->sale_price, 2, '.', '') }} ₺
-                    </div>
-                    <div class="btn sale-button">
-                        Sepete Ekle
-                    </div>
+                <div class="btn sale-button">
+                    Sepete Ekle
                 </div>
             </div>
-        </a>
+        </div>
     </div>
 @endforeach
 </div>
