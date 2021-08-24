02295a7c9f30275aab4643f15f349434177663a7
cenkgrs <cenkgrs@gmail.com>
2021-08-24 22:43:41 +0300

    css changes

--- resources/views/products/partials/list.blade.php
+++ resources/views/products/partials/list.blade.php
@@ -9,17 +9,15 @@
                     {{ $product->name }}
                 </div>
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-6 col-6">
                         <div class="product-colors">
-                            @if (!$product->colors->isEmpty())
-                                <div class="color-circle {{ $product->color }}"></div>
-                                @foreach ($product->colors as $color)
-                                    <div class="color-circle {{ $color->color }}"></div>
-                                @endforeach()
-                            @endif
+                            <div class="color-circle {{ $product->color }}"></div>
+                            @foreach ($product->colors as $color)
+                                <div class="color-circle {{ $color->color }}"></div>
+                            @endforeach()
                         </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-lg-6 col-6">
                         <div class="product-age">
                             {{ $product->age }}
                         </div>
