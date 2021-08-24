6629abdac5f761df95fbb38966e6408320f35858
cenkgrs <cenkgrs@gmail.com>
2021-08-24 21:24:00 +0300

    mobile font sizes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <footer>
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-sm-4 col-md-3 item">
+                <div class="col-sm-4 col-6 item">
                     <h3>bySura baby</h3>
                     <ul>
                         <li><a href="#"><span>Ürünler</span></a></li>
@@ -10,7 +10,7 @@
                         <li><a href="#"><span>İletişim</span></a></li>
                     </ul>
                 </div>
-                <div class="col-sm-4 col-md-3 item">
+                <div class="col-sm-4 col-6 item">
                     <h3>Hakkımızda</h3>
                     <ul>
                         <li><a href="#">Blog</a></li>
@@ -18,14 +18,14 @@
                         <li><a href="#">Sıkça Sorulan Sorular</a></li>
                     </ul>
                 </div>
-                <div class="col-sm-4 col-md-3 item">
+                <div class="col-sm-4 col-6 item">
                     <h3>Üyelik</h3>
                     <ul>
                         <li><a href="#">Hesabım</a></li>
                         <li><a href="#">Siparişlerim</a></li>
                     </ul>
                 </div>
-                <div class="col-lg-3 item social"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-twitter"></i></a>
+                <div class="col-lg-3 col-12 item social"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-twitter"></i></a>
                     <p class="copyright">BysuraBaby © 2021</p>
                 </div>
             </div>
