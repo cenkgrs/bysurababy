a2688a2aea32d259f6f53d7afd35a989378b0465
cenkgrs <cenkgrs@gmail.com>
2021-08-24 22:57:04 +0300

    Translate functions

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,28 +5,28 @@
                 <div class="col-sm-4 col-6 item">
                     <h3>bySura baby</h3>
                     <ul>
-                        <li><a href="#"><span>Ürünler</span></a></li>
-                        <li><a href="#"><span>Kategoriler</span></a></li
-                        <li><a href="#"><span>İletişim</span></a></li>
+                        <li><a href="#"><span>{{ __("Ürünler") }}</span></a></li>
+                        <li><a href="#"><span>{{ __("Kategoriler") }}</span></a></li
+                        <li><a href="#"><span>{{ __("İletişim") }}</span></a></li>
                     </ul>
                 </div>
                 <div class="col-sm-4 col-6 item">
                     <h3>Hakkımızda</h3>
                     <ul>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Vizyon</a></li>
-                        <li><a href="#">Sıkça Sorulan Sorular</a></li>
+                        <li><a href="#">{{ __("Blog") }}</a></li>
+                        <li><a href="#">{{ __("Vizyon") }}</a></li>
+                        <li><a href="#">{{ __("Sıkça Sorulan Sorular") }}</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-4 col-6 item">
-                    <h3>Üyelik</h3>
+                    <h3>{{ __("Üyelik") }}</h3>
                     <ul>
-                        <li><a href="#">Hesabım</a></li>
-                        <li><a href="#">Siparişlerim</a></li>
+                        <li><a href="#">{{ __("Hesabım") }}</a></li>
+                        <li><a href="#">{{ __("Siparişlerim") }}</a></li>
                     </ul>
                 </div>
                 <div class="col-lg-3 col-12 item social"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-twitter"></i></a>
-                    <p class="copyright">BysuraBaby © 2021</p>
+                    <p class="copyright">bySuraBaby © 2021</p>
                 </div>
             </div>
         </div>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,12 +4,12 @@
             <div class="col-lg-12">
                 <div class="upper-navbar">
                     <ul>
-                        <li><a href="/products"><span>Ürünler</span></a></li>
-                        <li><a href="/categories"><span>Kategoriler</span></a></li>
-                        <li><a href="/contact"><span>Blog</span></a></li>
-                        <li><a href="/contact"><span>Vizyon</span></a></li>
-                        <li><a href="/contact"><span>İletişim</span></a></li>
-                        <li><a href="/contact"><span>Siparişlerim</span></a></li>
+                        <li><a href="/products"><span>{{ __("Ürünler") }}</span></a></li>
+                        <li><a href="/categories"><span>{{ __("Kategoriler") }}</span></a></li>
+                        <li><a href="/contact"><span>{{ __("Blog") }}</span></a></li>
+                        <li><a href="/contact"><span>{{ __("Vizyon") }}</span></a></li>
+                        <li><a href="/contact"><span>{{ __("İletişim") }}</span></a></li>
+                        <li><a href="/contact"><span>{{ __("Siparişlerim") }}</span></a></li>
                     </ul>
                 </div>
 
@@ -24,8 +24,8 @@
                     <div class="col-lg-6 col-0" id="search-column">
                         <div class="nav-bar-search">
                             <i class="fa fa-search"></i>
-                            <input class="form-control" type="text" name="search" placeholder="Aramak istediğiniz ürünün ismini veya kategorisini giriniz...">
-                            <button class="btn search-button primary-button">Ara</button>
+                            <input class="form-control" type="text" name="search" placeholder="{{ __("Aramak istediğiniz ürünün ismini veya kategorisini giriniz") }}...">
+                            <button class="btn search-button primary-button">{{ __("Ara") }}</button>
                         </div>
                     </div>
                     <div class="col-lg-4 col-6">
@@ -34,12 +34,12 @@
                                 <div class="auth">
                                     @auth
                                         <button class="btn primary-button"><i class="fa fa-user"></i>
-                                            <span>Hesabım</span>
+                                            <span>{{ __("Hesabım") }}</span>
                                         </button>
                                     @endauth
                                     @guest
                                         <button class="btn primary-button"><i class="fa fa-user"></i>
-                                            <span>Giriş Yap / Üye Ol</span>
+                                            <span>{{ __("Giriş Yap / Üye Ol") }}</span>
                                         </button>
                                     @endguest
                                 </div>
@@ -47,7 +47,7 @@
                             <div class="col-lg-4 col-4">
                                 <div class="shopping-cart">
                                     <button class="btn primary-button"><i class="fa fa-cart-plus"></i>
-                                        <span>Sepetim</span>
+                                        <span>{{ __("Sepetim") }}</span>
                                     </button>
                                 </div>
                             </div>
