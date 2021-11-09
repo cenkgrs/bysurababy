adf8a40331e1b9b0890f36100803d976174daff8
Cenk Gürses <cenkgrs@gmail.com>
2021-11-09 23:47:34 +0300

    - mobile fix

--- resources/views/home/partials/stickers.blade.php
+++ resources/views/home/partials/stickers.blade.php
@@ -1,12 +1,12 @@
-<div class="row mb-5 mt-5">
+<div class="row mb-2 mt-5">
     @foreach ($stickers as $sticker)
-        <div class="col-lg-4">
+        <div class="col-lg-4 mb-3">
             <div class="sticker background-{{ $sticker['background'] }}">
                 <div class="row">
-                    <div class="col-lg-2 col-sm-2">
+                    <div class="col-lg-2 col-2">
                         <i class="{{ $sticker['icon'] }} sticker-icon color-{{ $sticker['background'] }}"></i>
                     </div>
-                    <div class="col-lg-10 col-sm-10">
+                    <div class="col-lg-10 col-10">
                         <div class="sticker-text {{ $sticker['color'] }}">
                             {{ $sticker['text'] }}
                         </div>
