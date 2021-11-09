498cfa50a0b056996a68c784536431716f46267d
Cenk Gürses <cenkgrs@gmail.com>
2021-11-09 22:31:28 +0300

    - category sticket mobile fix

--- resources/views/home/partials/category_stickers.blade.php
+++ resources/views/home/partials/category_stickers.blade.php
@@ -1,7 +1,7 @@
 <div class="category-stickers">
     <div class="row">
         @foreach ($category_stickers as $key => $category)
-            <div class="col-lg-1">
+            <div class="col-lg-1 col-2">
                 <a href="/products?category={{ $category->id }}">
                     <div class="category-sticker">
                         <img src="{{ asset('/images/products/'.$sticker_products[$key]->id.'.jpg') }}" alt="{{ $sticker_products[$key]->name }}">
