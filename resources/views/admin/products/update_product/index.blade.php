d227df55e64b0a6ecfd51f12112f1a6d103730cf
Cenk Gürses <cenkgrs@gmail.com>
2021-12-11 16:01:44 +0300

    - update message

--- resources/views/admin/products/update_product/index.blade.php
+++ resources/views/admin/products/update_product/index.blade.php
@@ -8,6 +8,12 @@
     </div>
 @endif
 
+@if(session()->has('success_message'))
+    <div class="alert alert-success">
+        {{ session()->get('success_message') }}
+    </div>
+@endif
+
 <form action="{{ route('admin.products.updateProductPost') }}" method="post">
 
     @csrf
