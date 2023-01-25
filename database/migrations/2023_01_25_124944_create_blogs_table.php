37bd56f2b1b4ab7cf150c9a374528d0e264b77a7
Cenk Gürses <cenkgrs@gmail.com>
2023-01-25 12:50:49 +0300

    blogs migration

---
+++ database/migrations/2023_01_25_124944_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug', 50);
+            $table->string('title', 50);
+            $table->string('description', 50);
+            $table->string('text', 11);
+            $table->string('banner', 50);
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
