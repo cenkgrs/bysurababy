8a814b6092a9fdee363605c8601c9e8750c5856d
cenkgrs <cenkgrs@gmail.com>
2023-01-31 23:59:47 +0300

    Categories and Contacts migration

---
+++ database/migrations/2023_01_31_235816_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('request_id', 50);
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('email', 50);
+            $table->string('phone', 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
