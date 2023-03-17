26124d5f22778c64f1b03c4927167e81f8bc3a67
Cenk Gürses <cenkgrs@gmail.com>
2023-03-17 18:25:34 +0300

    Check Driver Status

--- app/Http/Controllers/Api/DriverController.php
+++ app/Http/Controllers/Api/DriverController.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\ApiUsers;
-
+use App\Models\Deliveries;
 
 class DriverController extends Controller
 {
@@ -26,6 +26,24 @@ class DriverController extends Controller
         }
 
         return response()->json(["drivers" => $_drivers], 200);
+    }
+
+    public function checkDriverStatus(Request $request)
+    {
+        $input = $request->all();
+
+        // Get driver first
+        $driver = ApiUsers::where('user_type', 'driver')->where('id', $input['driver_id'])->first();
+
+        // Check if driver has any active delivery
+        $delivery = Deliveries::where('driver_id', $driver->id)->where('st_delivery', 1)->first();
+
+        // Driver is idle
+        if (!$delivery) {
+            return response()->json(["status" => 'passive'], 200);
+        }
+
+        return response()->json(["status" => 'active'], 200);
     }
 
 }
