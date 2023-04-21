3ac65576f990eb4ab15a495efc816c35cd8dcf5a
cenkgrs <cenkgrs@gmail.com>
2023-04-21 03:02:58 +0300

    routes revert

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
-use App\Http\Controllers\Admin\AuthController as AdminAuthController;
-use App\Http\Controllers\Admin\BlogController as AdminBlogController;
-use App\Http\Controllers\Admin\CategoriesController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ReportsController;
-use App\Http\Controllers\Admin\SeoController;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\User\FavoritesController;
-use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\User\FavoritesController;
 use App\Http\Controllers\User\ReviewController;
-use App\Http\Controllers\UserManagementController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -34,12 +20,12 @@ use App\Http\Controllers\UserManagementController;
 */
 
 // Home
-Route::get('/', HomeController::class, 'index')->name('index');
-Route::get('/index', HomeController::class, 'index')->name('index');
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('index');
+Route::get('/index', 'App\Http\Controllers\HomeController@index')->name('index');
 
 // Products
-Route::match(['get', 'post'], '/products', ProductsController::class, 'index')->name('products');
-Route::match(['get', 'post'], '/products/{product_id}', ProductsController::class, 'product')->name('product');
+Route::match(['get', 'post'], '/products', 'App\Http\Controllers\ProductsController@index')->name('products');
+Route::match(['get', 'post'], '/products/{product_id}', 'App\Http\Controllers\ProductsController@product')->name('product');
 
 // Cart
 Route::get('/cart', [CartController::class, "cart"])->name('cart');
@@ -48,44 +34,44 @@ Route::post('/remove-from-cart', [CartController::class, "removeFromCart"])->nam
 Route::post('/change-cart', [CartController::class, "changeProductQuantity"])->name('changeProductQuantity');
 
 // Booking
-Route::match(['get', 'post'], '/booking', BookingController::class, 'booking')->name('booking');
-Route::get('/finalize', BookingController::class, 'finalize')->name('finalize');
+Route::match(['get', 'post'], '/booking', 'App\Http\Controllers\BookingController@booking')->name('booking');
+Route::get('/finalize', 'App\Http\Controllers\BookingController@finalize')->name('finalize');
 
-Route::get('/categories', CategoryController::class, 'index')->name('categories');
+Route::get('/categories', 'App\Http\Controllers\CategoryController@index')->name('categories');
 
-Route::get('dashboard', AuthController::class, 'dashboard')->name('dashboard');
+Route::get('dashboard', 'App\Http\Controllers\Auth\AuthController@dashboard')->name('dashboard');
 
 // SEO Pages
-Route::get('/vizyon', PagesController::class, 'vision')->name('vision');
-Route::get('/misyon', PagesController::class, 'mission')->name('mission');
-Route::get('/iletisim', PagesController::class, 'contact')->name('contact');
-Route::get('/sss', PagesController::class, 'sss')->name('sss');
+Route::get('/vizyon', 'App\Http\Controllers\PagesController@vision')->name('vision');
+Route::get('/misyon', 'App\Http\Controllers\PagesController@mission')->name('mission');
+Route::get('/iletisim', 'App\Http\Controllers\PagesController@contact')->name('contact');
+Route::get('/sss', 'App\Http\Controllers\PagesController@sss')->name('sss');
 
 // Blogs
-Route::get('/blogs', BlogController::class, 'index')->name('blogs');
-Route::get('/blog/{slug}', BlogController::class, 'blog')->name('blog');
+Route::get('/blogs', 'App\Http\Controllers\BlogController@index')->name('blogs');
+Route::get('/blog/{slug}', 'App\Http\Controllers\BlogController@blog')->name('blog');
 
 // Partner
-Route::get('/partner', PagesController::class, 'partner')->name('partner');
-Route::post('/partner-application', PagesController::class, 'partnerApply')->name('partner-application');
+Route::get('/partner', 'App\Http\Controllers\PagesController@partner')->name('partner');
+Route::post('/partner-application', 'App\Http\Controllers\PagesController@partnerApply')->name('partner-application');
 
 /* USER MANAGEMENT */
 
 // Auth
-Route::match(['get', 'post'], '/login', AuthController::class, 'login')->name('login');
-Route::match(['get', 'post'], '/register', AuthController::class, 'register')->name('register');
-Route::get('/logout', AuthController::class, 'signOut')->name('logout');
+Route::match(['get', 'post'], '/login', 'App\Http\Controllers\Auth\AuthController@login')->name('login');
+Route::match(['get', 'post'], '/register', 'App\Http\Controllers\Auth\AuthController@register')->name('register');
+Route::get('/logout', 'App\Http\Controllers\Auth\AuthController@signOut')->name('logout');
 
 Route::group(['middleware' => 'checkUser'], function () {
     
     // Orders
-    Route::match(['get', 'post'], '/siparislerim', OrderController::class, 'index')->name('orders');
-    Route::get('/siparislerim/{request_id}', OrderController::class, 'order')->name('order');
+    Route::match(['get', 'post'], '/siparislerim', 'App\Http\Controllers\OrderController@index')->name('orders');
+    Route::get('/siparislerim/{request_id}', 'App\Http\Controllers\OrderController@order')->name('order');
 
     // Addresses
-    Route::get('/adres-bilgilerim', UserManagementController::class, 'addresses')->name('addresses');
-    Route::post('/adres-bilgilerim/adres-ekle', UserManagementController::class, 'addAddress')->name('addAddress');
-    Route::get('/adres-bilgilerim/adres-sil/{address_id}', UserManagementController::class, 'deleteAddress')->name('deleteAddress');
+    Route::get('/adres-bilgilerim', 'App\Http\Controllers\UserManagementController@addresses')->name('addresses');
+    Route::post('/adres-bilgilerim/adres-ekle', 'App\Http\Controllers\UserManagementController@addAddress')->name('addAddress');
+    Route::get('/adres-bilgilerim/adres-sil/{address_id}', 'App\Http\Controllers\UserManagementController@deleteAddress')->name('deleteAddress');
 
     // Reviews
     Route::match(['get', 'post'], '/degerlendirmelerim', [ReviewController::class, "index"])->name('reviews');
@@ -107,46 +93,46 @@ Route::group(['middleware' => 'checkUser'], function () {
 /* ADMIN */
 
 // Auth
-Route::match(['get', 'post'], '/panel/login', AdminAuthController::class, 'login')->name('admin.login');
-Route::get('/panel/logout', AdminAuthController::class, 'signOut')->name('admin.logout');
+Route::match(['get', 'post'], '/panel/login', 'App\Http\Controllers\Admin\AuthController@login')->name('admin.login');
+Route::get('/panel/logout', 'App\Http\Controllers\Admin\AuthController@signOut')->name('admin.logout');
 
 Route::group(['middleware' => 'checkAdmin'], function () {
-    Route::get('/panel', DashboardController::class, 'index')->name('admin.index');
-    Route::get('/panel/dashboard', DashboardController::class, 'index')->name('admin.index');
+    Route::get('/panel', 'App\Http\Controllers\Admin\DashboardController@index')->name('admin.index');
+    Route::get('/panel/dashboard', 'App\Http\Controllers\Admin\DashboardController@index')->name('admin.index');
     
     // Admin - Products
-    Route::match(['get', 'post'], 'panel/products', ProductsController::class, 'products')->name('admin.products');
-    Route::match(['get', 'post'], '/panel/add-product', ProductsController::class, 'addProduct')->name('admin.products.addProduct');
-    Route::get('/panel/update-product/{product_id}', ProductsController::class, 'updateProductGet')->name('admin.products.updateProductGet');
-    Route::post('/panel/update-product', ProductsController::class, 'updateProductPost')->name('admin.products.updateProductPost');
+    Route::match(['get', 'post'], 'panel/products', 'App\Http\Controllers\Admin\ProductController@products')->name('admin.products');
+    Route::match(['get', 'post'], '/panel/add-product', 'App\Http\Controllers\Admin\ProductController@addProduct')->name('admin.products.addProduct');
+    Route::get('/panel/update-product/{product_id}', 'App\Http\Controllers\Admin\ProductController@updateProductGet')->name('admin.products.updateProductGet');
+    Route::post('/panel/update-product', 'App\Http\Controllers\Admin\ProductController@updateProductPost')->name('admin.products.updateProductPost');
     
     // Categories
-    Route::match(['get', 'post'], '/panel/categories', CategoriesController::class, 'mainCategories')->name('admin.mainCategories');
-    Route::match(['get', 'post'], '/panel/sub-categories', CategoriesController::class, 'subCategories')->name('admin.subCategories');
-    Route::get('/panel/remove-category', CategoriesController::class, 'removeCategory')->name('admin.removeCategory');
+    Route::match(['get', 'post'], '/panel/categories', 'App\Http\Controllers\Admin\CategoriesController@mainCategories')->name('admin.mainCategories');
+    Route::match(['get', 'post'], '/panel/sub-categories', 'App\Http\Controllers\Admin\CategoriesController@subCategories')->name('admin.subCategories');
+    Route::get('/panel/remove-category', 'App\Http\Controllers\Admin\CategoriesController@removeCategory')->name('admin.removeCategory');
     
     // Reports
-    Route::match(['get', 'post'], '/panel/reports/sales', ReportsController::class, 'sales')->name('admin.reports.sales');
-    Route::match(['get', 'post'], '/panel/reports/sale/{request_id}', ReportsController::class, 'sale')->name('admin.reports.sale');
+    Route::match(['get', 'post'], '/panel/reports/sales', 'App\Http\Controllers\Admin\ReportsController@sales')->name('admin.reports.sales');
+    Route::match(['get', 'post'], '/panel/reports/sale/{request_id}', 'App\Http\Controllers\Admin\ReportsController@sale')->name('admin.reports.sale');
 
     // Users
-    Route::get('/panel/users', UserController::class, 'users')->name('admin.users');
-    Route::get('/panel/panel-users', UserController::class, 'panelUsers')->name('admin.panelUsers');
-    Route::get('/panel/panel-users/{user_id}', UserController::class, 'deletePanelUser')->name('admin.panelUsers.delete');
-    Route::post('/panel/panel-users/add', UserController::class, 'addPanelUser')->name('admin.panelUsers.add');
+    Route::get('/panel/users', 'App\Http\Controllers\Admin\UserController@users')->name('admin.users');
+    Route::get('/panel/panel-users', 'App\Http\Controllers\Admin\UserController@panelUsers')->name('admin.panelUsers');
+    Route::get('/panel/panel-users/{user_id}', 'App\Http\Controllers\Admin\UserController@deletePanelUser')->name('admin.panelUsers.delete');
+    Route::post('/panel/panel-users/add', 'App\Http\Controllers\Admin\UserController@addPanelUser')->name('admin.panelUsers.add');
 
     // Blogs
-    Route::get('/panel/blogs', AdminBlogController::class, 'index')->name('admin.blogs');
-    Route::get('/panel/blogs/update-blog/{blog_id}', AdminBlogController::class, 'updateBlogGet')->name('admin.blogs.updateBlogGet');
-    Route::post('/panel/blogs/update-blog', AdminBlogController::class, 'updateBlogPost')->name('admin.blogs.updateBlogPost');
-    Route::get('/panel/blogs/add-blog', AdminBlogController::class, 'addBlogGet')->name('admin.blogs.addBlogGet');
-    Route::post('/panel/blogs/add-blog', AdminBlogController::class, 'addBlogPost')->name('admin.blogs.addBlogPost');
+    Route::get('/panel/blogs', 'App\Http\Controllers\Admin\BlogController@index')->name('admin.blogs');
+    Route::get('/panel/blogs/update-blog/{blog_id}', 'App\Http\Controllers\Admin\BlogController@updateBlogGet')->name('admin.blogs.updateBlogGet');
+    Route::post('/panel/blogs/update-blog', 'App\Http\Controllers\Admin\BlogController@updateBlogPost')->name('admin.blogs.updateBlogPost');
+    Route::get('/panel/blogs/add-blog', 'App\Http\Controllers\Admin\BlogController@addBlogGet')->name('admin.blogs.addBlogGet');
+    Route::post('/panel/blogs/add-blog', 'App\Http\Controllers\Admin\BlogController@addBlogPost')->name('admin.blogs.addBlogPost');
 
     // SEO
-    Route::get('/panel/seo/texts', SeoController::class, 'seoTexts')->name('admin.seo.texts');
-    Route::match(['get', 'post'], '/panel/seo/add', SeoController::class, 'addSeoText')->name('admin.seo.addSeoText');
-    Route::match(['get', 'post'], '/panel/seo/update/{seo_id}', SeoController::class, 'updateSeoText')->name('admin.seo.updateSeoText');
-    Route::get('/panel/seo/delete/{seo_id}', SeoController::class, 'deleteSeoText')->name('admin.seo.deleteSeoText');
+    Route::get('/panel/seo/texts', 'App\Http\Controllers\Admin\SeoController@seoTexts')->name('admin.seo.texts');
+    Route::match(['get', 'post'], '/panel/seo/add', 'App\Http\Controllers\Admin\SeoController@addSeoText')->name('admin.seo.addSeoText');
+    Route::match(['get', 'post'], '/panel/seo/update/{seo_id}', 'App\Http\Controllers\Admin\SeoController@updateSeoText')->name('admin.seo.updateSeoText');
+    Route::get('/panel/seo/delete/{seo_id}', 'App\Http\Controllers\Admin\SeoController@deleteSeoText')->name('admin.seo.deleteSeoText');
 
     
 });
